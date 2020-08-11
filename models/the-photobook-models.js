44295055b32a4f6abbd380a5d24d4dd3027dcbf6
nisozakuto <nisozakuto@gmail.com>
2020-08-11 13:16:49 -0400

    model-getAll

---
+++ models/the-photobook-models.js
@@ -0,0 +1,22 @@
+const db = require('../db/config');
+
+class Picture {
+    constructor(pictures) {
+        this.id = pictures.id || null;
+        this.picUrl = pictures.picUrl;
+        this.albumId = pictures.albumId;
+        this.pic_desc = pictures.pic_desc;
+        this.liked = pictures.liked;
+    }
+
+    static getAll() {
+        return db.manyOrNone('SELECT * FROM pictures')
+            .then((picture) => {
+                return pictures.map((picture) => {
+                    return new this(picture);
+                })
+            })
+    }
+}
+
+module.exports = Picture;
