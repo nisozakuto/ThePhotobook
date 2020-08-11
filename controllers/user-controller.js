7303fafd2be6fe2b74971336a2030a6a29fa9a6c
nisozakuto <nisozakuto@gmail.com>
2020-08-11 17:01:42 -0400

    added Register form and it is connected

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,12 +2,14 @@ const User = require('../models/User-model')
 
 const usersController = {
     async new(req, res, next) {
-        const users = await User.getAll()
-        res.json({ users })
-
+        // const users = await User.getAll()
+        // res.json({ users })
+        res.render('users/register', {})
     },
     create(req, res, next) {
-        res.send('hello from photbook/create')
+        console.log(req.body)
+        res.send("you did it");
+        // res.send('hello from photbook/create')
     },
     indexPictures(req, res, next) {
         res.send('hello from photbook/index')
