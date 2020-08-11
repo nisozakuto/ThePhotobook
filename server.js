2f6a96504400059b867e9f0c94c2ac6b0ca45ecd
nisozakuto <nisozakuto@gmail.com>
2020-08-11 11:49:15 -0400

    Added some packages, connected to the '/'

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+const animalRouter = require('./routers/the-photobook-routers');
+
+const app = express();
+
+app.use(methodOverride('_method'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    //Test
+    // res.json({
+    //     message: "lets go",
+    // });
+    res.render('index', {
+        appName: 'The photobook'
+    });
+});
+
+app.use('*', (req, res) => {
+    res.status(404).send({
+        error: 'Not Found',
+    });
+    // res.render('404', {
+    //   });
+})
