52bb7612e1544d86f79fdb5f02c1040fd6a012dd
nisozakuto <nisozakuto@gmail.com>
2020-08-13 00:21:37 -0400

    Finally I got something from the API

--- routers/the-photobook-router.js
+++ routers/the-photobook-router.js
@@ -2,16 +2,21 @@ const express = require('express');
 const photobookRouter = express.Router();
 
 const photobookController = require('../controllers/the-photobook-controller');
-const getAPicture = require('../services/photo-helpers')
+const photoHelpers = require('../services/photo-helpers')
+const photoHelper = (req, res, next) => {
+    console.log(res.locals);
+    console.log(res.locals.picture);
+    console.log(res.locals.picture.pic_url)
+    next();
+}
 
 photobookRouter.get('/', photobookController.index);
 // photobookRouter.get('/:id([0-9]+)', photobookController.show);
 photobookRouter.post('/', photobookController.create);
 photobookRouter.delete('/:id([0-9]+)', photobookController.delete);
-
-photobookRouter.get('/:id([0-9]+)', photobookController.show, (req, res) => {
+photobookRouter.get('/:id([0-9]+)', photobookController.show, photoHelpers.getAPicture, (req, res) => {
     res.render('photobook/show', {
-        picture: res.locals.picture,
+        // picture: picture
     })
 });
 // photobookRouter.post('/', photobookController.create);
---
+++ services/photo-helpers.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+const getAPicture = (req, res, next) => {
+    // fetch(`https://api.pexels.com/v1/search?query=${res.locals.photo.src.medium}`, {
+    fetch(`https://api.pexels.com/v1/search?query=fish`, {
+        headers: {
+            Authorization: process.env.PEXELS_API_KEY,
+        },
+    })
+        .then((fetchRes) =>
+            fetchRes.json())
+        .then((allPictures) => {
+            console.log(allPictures);
+            // const picture = allPictures.photos[0].src.medium;
+            // res.locals.aPicture = picture;
+            next();
+        }).catch((err) => {
+            console.log(err);
+            next(err);
+        });
+};
+
+module.exports = {
+    getAPicture,
+};
