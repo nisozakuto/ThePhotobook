c585a757f45452047040f51548e23ca0b22dc35b
nisozakuto <nisozakuto@gmail.com>
2020-08-12 22:41:40 -0400

    styling and db_seed file update

--- db/migrations/20200811101000_seed.sql
+++ db/migrations/20200811101000_seed.sql
@@ -4,6 +4,6 @@ VALUES
     ('https://www.pexels.com/photo/bandwidth-close-up-computer-connection-1148820', 1 , 'You reached to the root of the Database', true),
     ('https://images.pexels.com/photos/1148572/pexels-photo-1148572.jpeg?auto=compress&cs=tinysrgb&h=350', 1, 'Love is everywhere', true),
     ('https://images.pexels.com/photos/3401900/pexels-photo-3401900.jpeg?auto=compress&cs=tinysrgb&h=350', 1, 'What a year', false),
-    ('https://images.pexels.com/photos/7367/startup-photo.jpg?auto=compress&cs=tinysrgb&h=350', 1, "APIs", TRUE),
-    ('https://images.pexels.com/photos/374710/pexels-photo-374710.jpeg?auto=compress&cs=tinysrgb&h=350', 2, "Manhattan", true),
-    ('https://images.pexels.com/photos/3800570/pexels-photo-3800570.jpeg?auto=compress&cs=tinysrgb&h=350https://images.pexels.com/photos/3800570/pexels-photo-3800570.jpeg?auto=compress&cs=tinysrgb&h=350', 2, "Cities", true);
+    ('https://images.pexels.com/photos/7367/startup-photo.jpg?auto=compress&cs=tinysrgb&h=350', 1, 'APIs', TRUE),
+    ('https://images.pexels.com/photos/374710/pexels-photo-374710.jpeg?auto=compress&cs=tinysrgb&h=350', 2, 'Manhattan', true),
+    ('https://images.pexels.com/photos/3800570/pexels-photo-3800570.jpeg?auto=compress&cs=tinysrgb&h=350', 2, 'Cities', true);
