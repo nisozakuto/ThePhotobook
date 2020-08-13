4e4dedf7c02d403d71ea418c2dc42edfce94d55e
nisozakuto <nisozakuto@gmail.com>
2020-08-13 15:22:33 -0400

    html and css and seed update

--- db/migrations/20200811101000_seed.sql
+++ db/migrations/20200811101000_seed.sql
@@ -1,9 +1,14 @@
 INSERT INTO pictures
-    (pic_url, album_id, pic_desc, liked)
+    (pic_url, album_id, pic_desc, liked, pic_url_full_size)
 VALUES
-    ('https://images.pexels.com/photos/373912/pexels-photo-373912.jpeg', 1 , 'You reached to the root of the Database', true),
-    ('https://images.pexels.com/photos/1148572/pexels-photo-1148572.jpeg?auto=compress&cs=tinysrgb&h=350', 1, 'Love is everywhere', true),
-    ('https://images.pexels.com/photos/3401900/pexels-photo-3401900.jpeg?auto=compress&cs=tinysrgb&h=350', 1, 'What a year', false),
-    ('https://images.pexels.com/photos/7367/startup-photo.jpg?auto=compress&cs=tinysrgb&h=350', 1, 'APIs', TRUE),
-    ('https://images.pexels.com/photos/374710/pexels-photo-374710.jpeg?auto=compress&cs=tinysrgb&h=350', 2, 'Manhattan', true),
-    ('https://images.pexels.com/photos/3800570/pexels-photo-3800570.jpeg?auto=compress&cs=tinysrgb&h=350', 2, 'Cities', true);
+    ('https://images.pexels.com/photos/1148820/pexels-photo-1148820.jpeg?auto=compress&cs=tinysrgb&h=350', 1 , 'You reached to the root of the Database', true, 'https://images.pexels.com/photos/1148820/pexels-photo-1148820.jpeg'),
+    ('https://images.pexels.com/photos/1148572/pexels-photo-1148572.jpeg?auto=compress&cs=tinysrgb&h=350', 1, 'Love is everywhere', true, 'https://images.pexels.com/photos/1148572/pexels-photo-1148572.jpeg'),
+    ('https://images.pexels.com/photos/3401900/pexels-photo-3401900.jpeg?auto=compress&cs=tinysrgb&h=350', 1, 'What a year', false, 'https://images.pexels.com/photos/3401900/pexels-photo-3401900.jpeg'),
+    ('https://images.pexels.com/photos/7367/startup-photo.jpg?auto=compress&cs=tinysrgb&h=350', 1, 'APIs', TRUE, 'https://images.pexels.com/photos/7367/startup-photo.jpg'),
+    ('https://images.pexels.com/photos/374710/pexels-photo-374710.jpeg?auto=compress&cs=tinysrgb&h=350', 2, 'Manhattan', true, 'https://images.pexels.com/photos/374710/pexels-photo-374710.jpeg'),
+    ('https://images.pexels.com/photos/17632/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350', 3, 'Cities', true, 'https://images.pexels.com/photos/17632/pexels-photo.jpg'),
+    ('https://images.pexels.com/photos/373912/pexels-photo-373912.jpeg?auto=compress&cs=tinysrgb&h=350', 3, 'Cities', true, 'https://images.pexels.com/photos/373912/pexels-photo-373912.jpeg'),
+    ('https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&h=350', 3, 'Cities', true, 'https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg'),
+    ('https://images.pexels.com/photos/1034662/pexels-photo-1034662.jpeg?auto=compress&cs=tinysrgb&h=350', 3, 'Cities', true, 'https://images.pexels.com/photos/1034662/pexels-photo-1034662.jpeg'),
+    ('https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&h=350', 3, 'Cities', true, 'https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg'),
+    ('https://images.pexels.com/photos/373893/pexels-photo-373893.jpeg?auto=compress&cs=tinysrgb&h=350', 3, 'Cities', true, 'https://images.pexels.com/photos/373893/pexels-photo-373893.jpeg');
