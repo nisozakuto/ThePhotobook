f09e10cde3dc763b1495f50064df6efdc8846da9
nisozakuto <nisozakuto@gmail.com>
2020-08-13 22:31:09 -0400

    creating the categories more efficiently

--- public/app.js
+++ public/app.js
@@ -15,7 +15,39 @@ function likedFunction() {
         liked = false;
 
     }
-
 }
+
+
+
+const categories = [
+    'Holding Hands',
+    'Wedding',
+    'People',
+    'Family',
+    'Couple',
+    'Love',
+    'Dark background',
+    'Happy birthday',
+    'Strategy'
+];
+
+
+const albumsContainer = document.querySelector('.albums-container');
+const col25 = document.querySelector('.col-25');
+
+if (albumsContainer) {
+    for (let category of categories) {
+        const ul = document.createElement('ul');
+        const li = document.createElement('li');
+        li.innerHTML = '';
+        li.innerHTML = '<form method="POST" action="/albums">' +
+            '<input class= "add-a-new-album" type = "submit" value = "' + category + '">' +
+            '<input type="hidden" name="name" value = "' + category + '">' +
+            '</form >';
+        ul.appendChild(li);
+        col25.appendChild(ul);
+    }
+}
+
 
 likedButton.addEventListener("click", likedFunction);
